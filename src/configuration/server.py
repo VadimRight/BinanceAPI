6636293fa56851f4a1a636f6dc869b49913b4e71
VadimRight <vadim.komashko@bk.ru>
2024-02-18 12:14:54 +0300

    Build server class is up

---
+++ src/configuration/server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi_utils.tasks import repeat_every
+from src.configuration.routes import __routes__
+from src.internal.event.startup import on_startup, on_loop_startup
+
+
+class Server:
+    __app: FastAPI
+
+    def __init__(self, app: FastAPI):
+        self.__app = app
+
+    def get_app(self) -> FastAPI:
+        return self.__app
+
+    @staticmethod
+    def __register_events(app):
+        app.on_event('startup')(repeat_every(seconds=60 * 60)(on_startup))
+        app.on_event('startup')(repeat_every(seconds=5)(on_loop_startup))
+
+    @staticmethod
+    def __register_route(app):
+        __routes__.register_router(app)
