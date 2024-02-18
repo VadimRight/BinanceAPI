8c64dc84ed5d3e6f80f1a00186e9855ef20663ab
VadimRight <vadim.komashko@bk.ru>
2024-02-18 11:49:58 +0300

    Fit: startup events for getiing pairs are up

---
+++ src/internal/event/startup.py
@@ -0,0 +1,24 @@
+import aiohttp
+
+from src.configuration.config import ALL_PAIRS_KEY, CURRENCY_PAIR_KEY
+from src.internal.schemas.currency import Symbols
+from src.pkg.redis_tools.tools import RedisTools
+
+
+async def on_startup():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(ALL_PAIRS_KEY) as responce:
+            response_json = await responce.json()
+            parsed_pairs = Symbols(**response_json)
+            cut_pairs = parsed_pairs.symbols[:20]
+            symbols = [pair.symbol for pair in cut_pairs]
+            for symbol in symbols:
+                RedisTools.get_pair(symbol, 0)
+
+
+async def on_loop_startup():
+    for symbol in RedisTools.get_keys():
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{CURRENCY_PAIR_KEY}{symbol.decode("utf-8")}') as response:
+                response_json = await response.json()
+                RedisTools.set_pair(symbol, response_json['price'])
